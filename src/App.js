799e1cb44366994f02fb2f36d55a5804a8329362
ethan sayagh <ethan.sayagh@gmail.com>
2020-12-22 11:39:25 +0200

    placed text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ function App() {
   return (
     <div className='generalContainer'>
       <div className='square'>
-        <div className='square-content'>Hello!</div>
+        <div className='square-content'>
+          <h1>Zero Sugar</h1>
+          <p>
+            Coca-Cola Zero Sugar
+            <br /> Real Coke taste with zero
+            <br /> calories. It's possible!
+          </p>
+        </div>
       </div>
     </div>
   );
