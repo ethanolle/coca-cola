baa5a211e57c23ed8538bb1661c10dd801872c8c
ethan sayagh <ethan.sayagh@gmail.com>
2020-12-27 21:15:25 +0200

    added text animation plus a json file for the text

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import "./App.scss";
+import "./scss/App.scss";
 import React, { useState } from "react";
+import info from "./const/coca_description.json";
 
 function App() {
   const [transition, setTransition] = useState(false);
 
   return (
     <div className='generalContainer'>
-      <img alt='cocacola' className='coca-logo center-page ' />
+      <img alt='cocacola' className='coca-logo  ' />
       <img
         alt='cocacola'
         onAnimationEnd={() => {
           setTransition("right");
         }}
         className={
-          transition === true ? "products slide-animation " : "products  "
+          transition === true ? "products slide-animation-left " : "products  "
         }
         src='/images/Zero.png'
       />
-      <div className='square center-page '>
+      <div className='square  '>
         <img alt='next-button' className='next-container' />
         <h2 className='next-button' onClick={() => setTransition(true)}>
           Next
         </h2>
-        <div className='square-content'>
-          <h1>Zero Sugar</h1>
+        <div
+          className={
+            transition === true
+              ? "square-content slide-animation-down "
+              : "square-content  "
+          }
+        >
+          <h1>{info.data[0].title}</h1>
           <p>
-            Coca-Cola Zero Sugar
-            <br /> Real Coke taste with zero
-            <br /> calories. It's possible!
+            {info.data[0]["text-line1"]}
+            <br /> {info.data[0]["text-line2"]}
+            <br /> {info.data[0]["text-line3"]}
           </p>
         </div>
       </div>
